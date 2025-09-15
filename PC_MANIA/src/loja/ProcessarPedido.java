8bca081750d1d9514cbc669c70fd37ed606aa5a5
João Pedro Torrano Dias <130389414+JoaoDTorrano@users.noreply.github.com>
2025-09-15 17:21:28 -0300

    Add files via upload

--- PC_MANIA/src/loja/ProcessarPedido.java
+++ PC_MANIA/src/loja/ProcessarPedido.java
@@ -4,6 +4,5 @@ public class ProcessarPedido {
 
     public static void enviarPedido() {
         System.out.println("Obrigado por sua compra! Seu pedido está sendo processado.");
-        // Você pode adicionar mais lógica aqui se necessário no futuro.
     }
 }
