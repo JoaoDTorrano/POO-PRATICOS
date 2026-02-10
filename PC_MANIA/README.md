7c9e39357dcd8a6502180d2bb0fd4b2fa2030195
João Pedro Torrano Dias <130389414+JoaoDTorrano@users.noreply.github.com>
2026-02-10 13:46:06 -0300

    Delete PC_MANIA directory

--- PC_MANIA/README.md
+++
@@ -1,3 +0,0 @@
-NOME: João Pedro Torrano Dias / 
-CURSO: GES / 
-MATRÍCULA: 273
--- PC_MANIA/src/loja/ProcessarPedido.java
+++
@@ -1,8 +0,0 @@
-package loja;
-
-public class ProcessarPedido {
-
-    public static void enviarPedido() {
-        System.out.println("Obrigado por sua compra! Seu pedido está sendo processado.");
-    }
-}
